ccefdcde2127facc95b7d40db859f7f993747599
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-03 23:26:21 +0530

    routing for homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {  Box, Drawer, List, ListItem, ListItemText } from "@mui/material";
+import Alerts  from './pages/Alerts'
+import DeviceInventory from './pages/DeviceInventory'
+import AMCTracker from './pages/AMCTracker'
+import Installation from './pages/Installations'
+import ServiceLogs from "./pages/ServiceLogs";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Box sx={{ display: 'flex' }}>
+          <Drawer variant="permanent" sx={{ width: 220, [`& .MuiDrawer-paper`]: { width: 220, boxSizing: 'border-box',alignItems: "center" } }}>
+            <List>
+              <ListItem button component={Link} to="/">
+                <ListItemText primary="Device Inventory" />
+              </ListItem>
+              <ListItem button component={Link} to="/installations">
+                <ListItemText primary="Installations & Training" />
+              </ListItem>
+              <ListItem button component={Link} to="/service-logs">
+                <ListItemText primary="Service Visit Logs" />
+              </ListItem>
+              <ListItem button component={Link} to="/amc-tracker">
+                <ListItemText primary="AMC/CMC Tracker" />
+              </ListItem>
+              <ListItem button component={Link} to="/alerts">
+                <ListItemText primary="Alerts & Photo Logs" />
+              </ListItem>
+            </List>
+          </Drawer>
+          <Box>
+            <Routes>
+            <Route path="/" element={<DeviceInventory/>}/>
+            <Route path="/installation" element={<Installation/>}/>
+            <Route path="/service-logs" element={<ServiceLogs/>}/>
+            <Route path="/amc-tracker" element={<AMCTracker/>}/>
+            <Route path="/alert" element={<Alerts/>}/>
+
+            </Routes>
+          </Box>
+        </Box>
+      </Router>
+    </>
   );
 }
 
